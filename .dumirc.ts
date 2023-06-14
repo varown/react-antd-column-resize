d4ccc07279ad71f6a63a78101e150b724645809d
mengpeng <mengpeng@app315.net>
2023-06-14 17:30:55 +0800

    chore: github action

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'dumi';
-
+const repo = 'react-antd-column-resize'; // 项目名
 export default defineConfig({
   outputPath: 'docs-dist',
   themeConfig: {
     name: ' ',
   },
+  base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
 });
